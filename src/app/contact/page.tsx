bb5ba369c0946a49b7dc17707c9c6b22021b10c2
muhammod1 <muhammodholawale@gmail.com>
2023-07-04 21:56:33 +0100

    Contact us page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,7 +6,8 @@ import { BiUser } from "react-icons/bi";
 import { MdEmail } from "react-icons/md";
 import {
   AiFillInstagram,
-  AiOutlineTwitter,AiOutlineFileText,
+  AiOutlineTwitter,
+  AiOutlineFileText,
   AiFillLinkedin,
 } from "react-icons/ai";
 import { BsFacebook, BsTelephone } from "react-icons/bs";
@@ -82,12 +83,72 @@ export default function Home() {
 
       <div className="my-20">
         <h1 className="text-5xl font-bold text-center text-app-sblue">
-          Get <span className="text-[#fff]">in </span> <span className="text-app-porange">touch</span>
+          Get <span className="text-[#fff]">in </span>{" "}
+          <span className="text-app-porange">touch</span>
         </h1>
-        <form>
-            <div>
-                <input type="fname" placeholder="First Name"  />
+
+        <form className="tablet_l:w-[600px] w-[95%] mx-auto mt-10 mb-20">
+          <div className="flex justify-between gap-5 flex-col tablet_l:flex-row">
+            <div className="flex justify-between items-center space-x-2 p-2 border-2 border-neutral-700/60 dark:border-white/40 rounded-md">
+              <BiUser className="text-xl text-neutral-900/50 dark:text-white" />
+              <input
+                type="text"
+                className="flex-grow bg-transparent text-neutral-900 dark:text-white outline-none"
+                // onChange={e => setEmail(e.target.value)}
+                // value={email}
+                placeholder="First Name"
+              />
             </div>
+            <div className="flex justify-between items-center space-x-2 p-2 border-2 border-neutral-700/60 dark:border-white/40 rounded-md">
+              <BiUser className="text-xl text-neutral-900/50 dark:text-white" />
+              <input
+                type="text"
+                className="flex-grow bg-transparent text-neutral-900 dark:text-white outline-none"
+                // onChange={e => setEmail(e.target.value)}
+                // value={email}
+                placeholder="Last Name"
+              />
+            </div>
+          </div>
+          <br />
+          <div className="flex justify-between items-center space-x-2 p-2 border-2 border-neutral-700/60 dark:border-white/40 rounded-md">
+              <MdEmail className="text-xl text-neutral-900/50 dark:text-white" />
+              <input
+                type="text"
+                className="flex-grow bg-transparent text-neutral-900 dark:text-white outline-none"
+                // onChange={e => setEmail(e.target.value)}
+                // value={email}
+                placeholder="Enter your email"
+              />
+            </div>
+          <br />
+            <div className="flex justify-between items-center space-x-2 p-2 border-2 border-neutral-700/60 dark:border-white/40 rounded-md">
+              <BsTelephone className="text-xl text-neutral-900/50 dark:text-white" />
+              <input
+                type="text"
+                className="flex-grow bg-transparent text-neutral-900 dark:text-white outline-none"
+                // onChange={e => setEmail(e.target.value)}
+                // value={email}
+                placeholder="Enter your phone number"
+              />
+            </div>
+            <br />
+            <div className="flex justify-between items- space-x-2 p-2 border-2 border-neutral-700/60 dark:border-white/40 rounded-md">
+              <AiOutlineFileText className="text-xl text-neutral-900/50 dark:text-white" />
+              <textarea
+                // type="text"
+                className="flex-grow bg-transparent h-[200px] text-neutral-900 dark:text-white outline-none"
+                // onChange={e => setEmail(e.target.value)}
+                // value={email}
+                placeholder="Write your message?"
+              />
+            </div>
+            <button
+                className="flex items-center gap-3 px-10 py-3 mt-7 mx-auto w-fit bg-app-sblue text-[14px] laptop:text[16px]  rounded-3xl"
+              >
+                Submit
+                <BsArrowRight className="text-md" />
+              </button>
         </form>
       </div>
     </section>
