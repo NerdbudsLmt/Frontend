c069340e65f16f562595141af76b1af9957be57f
muhammod1 <muhammodholawale@gmail.com>
2023-07-04 21:27:01 +0100

    about us page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,14 +2,14 @@ import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { BsArrowRight } from "react-icons/bs";
-import { BiMobileAlt } from "react-icons/bi";
+import { BiUser } from "react-icons/bi";
 import { MdEmail } from "react-icons/md";
 import {
   AiFillInstagram,
-  AiOutlineTwitter,
+  AiOutlineTwitter,AiOutlineFileText,
   AiFillLinkedin,
 } from "react-icons/ai";
-import { BsFacebook } from "react-icons/bs";
+import { BsFacebook, BsTelephone } from "react-icons/bs";
 
 export default function Home() {
   return (
@@ -49,8 +49,8 @@ export default function Home() {
         priority
       />
 
-      <div className="flex items-center">
-        <div className="border-r-2">
+      <div className="flex items-center w-fit mx-auto my-20">
+        <div className="border-r-2 pr-10 ">
           <h1 className="text-2xl font-bold  text-app-sblue">
             Follow <span className="text-app-porange">us</span>
           </h1>
@@ -61,9 +61,9 @@ export default function Home() {
             <AiOutlineTwitter className="text-[#fff] w-[20px] h-[20px]" />
           </div>
         </div>
-        <div className="h-full w-[1px]"/>
+        {/* <div className="h-full w-[4px] mx-7 bg-[#fff]"/> */}
 
-        <div>
+        <div className="pl-10">
           <Link
             href="/"
             className=" border-[#3F9BD5] bg-[#3F9BD5] text-white text-[14px] mx-auto flex gap-2 items-center border-2 w-fit mt-6 tablet_l:mt-0 py-2 px-6 rounded-full"
@@ -78,6 +78,17 @@ export default function Home() {
             <span>+234 904 900 4960</span>
           </Link>
         </div>
+      </div>
+
+      <div className="my-20">
+        <h1 className="text-5xl font-bold text-center text-app-sblue">
+          Get <span className="text-[#fff]">in </span> <span className="text-app-porange">touch</span>
+        </h1>
+        <form>
+            <div>
+                <input type="fname" placeholder="First Name"  />
+            </div>
+        </form>
       </div>
     </section>
   );
