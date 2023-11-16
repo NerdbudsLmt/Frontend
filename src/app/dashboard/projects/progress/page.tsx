1e6ff36526cf472f83c8d504f883e83b2d8713b8
muhammod1 <muhammodholawale@gmail.com>
2023-11-16 16:15:23 +0100

    latest update

--- src/app/dashboard/projects/progress/page.tsx
+++ src/app/dashboard/projects/progress/page.tsx
@@ -8,10 +8,6 @@ import {
   MenuButton,
   MenuList,
   MenuItem,
-  MenuItemOption,
-  MenuGroup,
-  MenuOptionGroup,
-  MenuDivider,
   Button
 } from "@chakra-ui/react";
 import Pagination from "@/components/Pagination";
