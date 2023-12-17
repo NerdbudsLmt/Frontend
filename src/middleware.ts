3d9254944e99faf6c6b18e0d316902ce8723c89b
muhammod1 <muhammodholawale@gmail.com>
2023-12-17 07:20:00 +0100

    authentication implementation

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,25 @@ export default withAuth(
         console.log(request.nextUrl.pathname)
         console.log(request.nextauth.token)
 
-      
+        if (request.nextUrl.pathname.startsWith("/")
+            && request.nextauth.token?.userType !== "Company"
+            && request.nextauth.token?.userType !== "Student"
+                && request.nextauth.token?.userType !== "Business"
+                && request.nextauth.token?.userType !== ""
+            ) {
+            return NextResponse.rewrite(
+                new URL("/not-found", request.url)
+            )
+        }
+
+        // if (request.nextUrl.pathname.startsWith("/")
+        //     && request.nextauth.token?.userType !== "Student"
+        //     && request.nextauth.token?.userType !== "Business"
+        // ) {
+        //     return NextResponse.rewrite(
+        //         new URL("/not-found", request.url)
+        //     )
+        // }
     },
     {
         callbacks: {
