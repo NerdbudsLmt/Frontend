91f6fe53d28c5e3e24bbdf6a8adb69da754c5355
muhammod1 <muhammodholawale@gmail.com>
2023-12-21 09:07:49 +0100

    added some changes into the middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,30 +2,30 @@ import { withAuth, NextRequestWithAuth } from "next-auth/middleware"
 import { NextResponse } from "next/server"
 
 export default withAuth(
-    // function middleware(request: NextRequestWithAuth) {
-    //     // console.log(request.nextUrl.pathname)
-    //     // console.log(request.nextauth.token)
+    function middleware(request: NextRequestWithAuth) {
+        // console.log(request.nextUrl.pathname)
+        // console.log(request.nextauth.token)
 
-    //     if (request.nextUrl.pathname.startsWith("/")
-    //         && request.nextauth.token?.userType !== "Company"
-    //         && request.nextauth.token?.userType !== "Student"
-    //             && request.nextauth.token?.userType !== "Business"
+        if (request.nextUrl.pathname.startsWith("/dashboard")
+            && request.nextauth.token?.userType == "Company"
+            && request.nextauth.token?.userType == "Student"
+                && request.nextauth.token?.userType == "Business"
                
-    //         ) {
-    //         return NextResponse.rewrite(
-    //             new URL("/not-found", request.url)
-    //         )
-    //     }
+            ) {
+            return NextResponse.rewrite(
+                new URL("/not-found", request.url)
+            )
+        }
 
-    //     // if (request.nextUrl.pathname.startsWith("/")
-    //     //     && request.nextauth.token?.userType !== "Student"
-    //     //     && request.nextauth.token?.userType !== "Business"
-    //     // ) {
-    //     //     return NextResponse.rewrite(
-    //     //         new URL("/not-found", request.url)
-    //     //     )
-    //     // }
-    // },
+        // if (request.nextUrl.pathname.startsWith("/")
+        //     && request.nextauth.token?.userType !== "Student"
+        //     && request.nextauth.token?.userType !== "Business"
+        // ) {
+        //     return NextResponse.rewrite(
+        //         new URL("/not-found", request.url)
+        //     )
+        // }
+    },
     {
         callbacks: {
             authorized: ({ token }) => !!token
