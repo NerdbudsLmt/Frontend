c920c26212c1e4ac21e5f1060e96330c52c42520
muhammod1 <muhammodholawale@gmail.com>
2023-12-21 08:18:45 +0100

    added some changes into the middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,25 +6,17 @@ export default withAuth(
         console.log(request.nextUrl.pathname)
         console.log(request.nextauth.token)
 
-        if (request.nextUrl.pathname.startsWith("/")
+        if (request.nextUrl.pathname.startsWith("/dashboard")
             && request.nextauth.token?.userType !== "Company"
             && request.nextauth.token?.userType !== "Student"
                 && request.nextauth.token?.userType !== "Business"
-                && request.nextauth.token?.userType !== ""
+               
             ) {
             return NextResponse.rewrite(
                 new URL("/not-found", request.url)
             )
         }
 
-        // if (request.nextUrl.pathname.startsWith("/")
-        //     && request.nextauth.token?.userType !== "Student"
-        //     && request.nextauth.token?.userType !== "Business"
-        // ) {
-        //     return NextResponse.rewrite(
-        //         new URL("/not-found", request.url)
-        //     )
-        // }
     },
     {
         callbacks: {
@@ -35,16 +27,4 @@ export default withAuth(
 
 export const config = { matcher: [
     '/dashboard/:path*',
-    "/dashboard", 
-    "/dashboard/projects", 
-    "/dashboard/projects/create", 
-    "/dashboard/projects/existing", 
-    "/dashboard/projects/finished", 
-    "/dashboard/projects/progress", 
-    "/dashboard/payment/comfirmation", 
-    "/dashboard/payment/paymmethod", 
-    "/dashboard/payment/transaction", 
-    "/dashboard/payment", 
-    "/dashboard/settings", 
-    "/dashboard/support", 
 ] }
