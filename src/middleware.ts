2812438a8bcfe15a7a55bffdcb79073d777bb673
muhammod1 <muhammodholawale@gmail.com>
2023-12-22 17:52:33 +0100

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,44 +1,61 @@
-import { withAuth, NextRequestWithAuth } from "next-auth/middleware"
-import { NextResponse } from "next/server"
+// import { withAuth, NextRequestWithAuth } from "next-auth/middleware"
+// import { NextResponse } from "next/server"
 
-export default withAuth(
-    function middleware(request: NextRequestWithAuth) {
-        // console.log(request.nextUrl.pathname)
-        // console.log(request.nextauth.token)
+// export default withAuth(
+//     function middleware(request: NextRequestWithAuth) {
+//         // console.log(request.nextUrl.pathname)
+//         // console.log(request.nextauth.token)
 
-        // if (request.nextUrl.pathname.startsWith("/dashboard")
-        //     && request.nextauth.token?.userType !== "Company"
-        //     && request.nextauth.token?.userType !== "Student"
-        //         && request.nextauth.token?.userType !== "Business"
-        //         // && request.nextauth.token?.userType !== ""
+//         // if (request.nextUrl.pathname.startsWith("/dashboard")
+//         //     && request.nextauth.token?.userType !== "Company"
+//         //     && request.nextauth.token?.userType !== "Student"
+//         //         && request.nextauth.token?.userType !== "Business"
+//         //         // && request.nextauth.token?.userType !== ""
                
-        //     ) {
-        //     // return NextResponse.rewrite(
-        //     //     new URL("/not-found", request.url)
-        //     // )
-        // }
+//         //     ) {
+//         //     // return NextResponse.rewrite(
+//         //     //     new URL("/not-found", request.url)
+//         //     // )
+//         // }
 
         
-    },
-    {
-        callbacks: {
-            authorized: ({ token }) => !!token
-        },
-    }
-)
+//     },
+//     {
+//         callbacks: {
+//             authorized: ({ token }) => !!token
+//         },
+//     }
+// )
+
+// export const config = { matcher: [
+//     // '/dashboard/:path*',
+//     "/dashboard", 
+//     "/dashboard/projects", 
+//     "/dashboard/projects/create", 
+//     "/dashboard/projects/existing", 
+//     "/dashboard/projects/finished", 
+//     "/dashboard/projects/progress", 
+//     "/dashboard/payment/comfirmation", 
+//     "/dashboard/payment/paymmethod", 
+//     "/dashboard/payment/transaction", 
+//     "/dashboard/payment", 
+//     "/dashboard/settings", 
+//     "/dashboard/support", 
+// ] }
+
+export { default } from "next-auth/middleware"
 
 export const config = { matcher: [
-    // '/dashboard/:path*',
-    "/dashboard", 
-    "/dashboard/projects", 
-    "/dashboard/projects/create", 
-    "/dashboard/projects/existing", 
-    "/dashboard/projects/finished", 
-    "/dashboard/projects/progress", 
-    "/dashboard/payment/comfirmation", 
-    "/dashboard/payment/paymmethod", 
-    "/dashboard/payment/transaction", 
-    "/dashboard/payment", 
-    "/dashboard/settings", 
-    "/dashboard/support", 
+    // "/dashboard", 
+        "/dashboard/projects", 
+        "/dashboard/projects/create", 
+        "/dashboard/projects/existing", 
+        "/dashboard/projects/finished", 
+        "/dashboard/projects/progress", 
+        "/dashboard/payment/comfirmation", 
+        "/dashboard/payment/paymmethod", 
+        "/dashboard/payment/transaction", 
+        "/dashboard/payment", 
+        "/dashboard/settings", 
+        "/dashboard/support", 
 ] }
