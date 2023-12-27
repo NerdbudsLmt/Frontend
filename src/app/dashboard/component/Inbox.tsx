3a05bd3abf2c66db5feb3d65f14185e5074e4d08
Samuel <Samueladeyemi2006@gmail.com>
2023-12-27 15:58:06 +0100

    feat: tawk.to integration

--- src/app/dashboard/component/Inbox.tsx
+++ src/app/dashboard/component/Inbox.tsx
@@ -1,25 +1,25 @@
-import React, { useState } from 'react'
-import InboxModal from './InboxModal'
-import { HiOutlineChatBubbleLeftEllipsis } from 'react-icons/hi2'
+import React, { useState } from "react";
+import InboxModal from "./InboxModal";
+import { HiOutlineChatBubbleLeftEllipsis } from "react-icons/hi2";
 
 export const Inbox = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModal = () => {
-    setIsModalOpen(true)
-  }
+    setIsModalOpen(true);
+  };
 
   const closeModal = () => {
-    setIsModalOpen(false)
-  }
+    setIsModalOpen(false);
+  };
   return (
     <div>
       <HiOutlineChatBubbleLeftEllipsis
         size={25}
         onClick={openModal}
-        style={{ cursor: 'pointer' }}
+        style={{ cursor: "pointer" }}
       />
       <InboxModal isOpen={isModalOpen} onClose={closeModal} />
     </div>
-  )
-}
+  );
+};
