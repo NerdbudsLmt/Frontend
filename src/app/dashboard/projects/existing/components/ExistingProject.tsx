484745a76dba0e085215e6ae88dbf33e9c87c829
Gideon Atauba <ataubagideon@gmail.com>
2023-12-30 12:44:19 +0100

    existing project

--- src/app/dashboard/projects/existing/components/ExistingProject.tsx
+++ src/app/dashboard/projects/existing/components/ExistingProject.tsx
@@ -1,148 +1,161 @@
 'use client'
 
-import Pagination from "@/components/Pagination";
-import React, { useState } from "react";
-import { MdOutlineKeyboardArrowDown } from "react-icons/md";
-import { RiDeleteBinLine } from "react-icons/ri";
-
+import Pagination from '@/components/Pagination'
+import React, { useState, useEffect } from 'react'
+import { MdOutlineKeyboardArrowDown } from 'react-icons/md'
+import { RiDeleteBinLine } from 'react-icons/ri'
+import { useSession } from 'next-auth/react'
 
 interface Project {
-  title: string;
-  status: "Active" | "Not Active" | "Finished";
-  id: number;
+  title: string
+  status: 'Active' | 'Not Active' | 'Finished'
+  id: number
 }
-const projectList: Project[] = [
-  {
-    title: "Project Assistant",
-    status: "Active",
-    id: 1,
-  },
-  {
-    title: "Brand IT",
-    status: "Not Active",
-    id: 2,
-  },
-  {
-    title: "Nerdburds Pro",
-    status: "Active",
-    id: 3,
-  },
-  {
-    title: "Project Assistant",
-    status: "Active",
-    id: 4,
-  },
-  {
-    title: "Brand IT",
-    status: "Not Active",
-    id: 5,
-  },
-  {
-    title: "Nerdburds Pro",
-    status: "Active",
-    id: 6,
-  },
-  {
-    title: "Project Assistant",
-    status: "Active",
-    id: 7,
-  },
-  {
-    title: "Brand IT",
-    status: "Not Active",
-    id: 8,
-  },
-  {
-    title: "Nerdburds Pro",
-    status: "Active",
-    id: 9,
-  },
-];
+
 
 const ExistingProject: React.FC = () => {
+  const { data: session } = useSession()
+  const [projects, setProjects] = useState<Project[]>([])
+  const [currentPage, setCurrentPage] = useState(1)
+  const [postsPerPage] = useState(5)
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(5);
+  useEffect(() => {
+    const fetchUserProjects = async () => {
+      try {
+        const accessToken = session?.user?.accessToken ?? ''
+
+        if (!accessToken) {
+          console.error('Access token not available')
+          return
+        }
+
+        const url = 'https://nerdbuds.onrender.com/api/v1/projects/userProjects'
+
+        const response = await fetch(url, {
+          method: 'GET',
+          headers: {
+            // 'Content-Type': 'application/json',
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+
+        console.log('API Response:', response)
+
+        if (response.ok) {
+          const data = await response.json()
+          setProjects(data.projects)
+        } else {
+          console.error('Failed to fetch user projects')
+        }
+      } catch (error) {
+        console.error('An error occurred:', error)
+      }
+    }
+
+    fetchUserProjects()
+  }, [session])
 
 
-  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage
+  const indexOfFirstPost = indexOfLastPost - postsPerPage
+  const currentPosts = projects?.slice(indexOfFirstPost, indexOfLastPost)
 
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = projectList.slice(indexOfFirstPost, indexOfLastPost);
-
-  //Change page
-  // const paginate = (pageNumber: any) => setCurrentPage(pageNumber);
-  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
-  const paginateFront = () => setCurrentPage(currentPage + 1);
-  const paginateBack = () =>{ 
-    setCurrentPage(currentPage - 1)
-  }
-
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber)
+  const paginateFront = () => setCurrentPage(currentPage + 1)
+  const paginateBack = () => setCurrentPage(currentPage - 1)
 
   return (
-    <div className="max-w-[1000px] ">
-      <div className="flex gap-2 mb-8 items-center">
-        <p className="text-gray-500 text-xl font-semibold">Project</p>
-        <div className="h-2 w-2 bg-black rounded-full" />
-        <p className="font-semibold  text-2xl">Existing project</p>
+    <div className='max-w-[1000px] '>
+      <div className='flex gap-2 mb-8 items-center'>
+        <p className='text-gray-500 text-xl font-semibold'>Project</p>
+        <div className='h-2 w-2 bg-black rounded-full' />
+        <p className='font-semibold  text-2xl'>Existing project</p>
       </div>
-
-      <div className="mt-10  text-white list-decimal  text-md">
-        {currentPosts.map((item) => (
-          <div
-            key={item.id}
-            className="flex items-center justify-between flex-wrap rounded-lg py-2 px-4 gap-4 my-5 bg-[#F5F4F4]"
-          >
-            {/* <p className="text-[18px]">{item.id}</p> */}
-            <div className="flex items-center gap-4">
-              <p className="text-[18px] border-r-2 border-black pr-4">
-                {item.title}
-              </p>
-              <p
-                className={
-                  item.status === "Active"
-                    ? "text-[14px] font-semibold text-[#5583C3]"
-                    : "text-[14px] font-semibold text-gray-400"
-                }
+      {projects?.length === 0 ? (
+        <p>No project Available</p>
+      ) : (
+        <>
+          <div className='mt-10  text-white list-decimal  text-md'>
+            {currentPosts?.map((item) => (
+              <div
+                key={item.id}
+                className='flex items-center justify-between flex-wrap rounded-lg py-2 px-4 gap-4 my-5 bg-[#F5F4F4]'
               >
-                {item.status}
-              </p>
-            </div>
-            <div className="flex gap-3 flex-wrap ">
-              <button className="flex gap-2 items-center bg-[#DFDFDF] py-2 px-2 rounded-lg">
-                Request delete
-                <RiDeleteBinLine />
-              </button>
-              {item.status === 'Not Active' && (
-                <button className="flex gap-2 items-center bg-[#5583C3] text-white py-2 px-3 rounded-lg">
-                  Activate
-                </button>
-              )}
-              <button className="flex gap-2 items-center bg-[#C8C8C8] py-2 px-2 rounded-lg">
-                See more
-                <MdOutlineKeyboardArrowDown />
-              </button>
-            </div>
+                {/* <p className="text-[18px]">{item.id}</p> */}
+                <div className='flex items-center gap-4'>
+                  <p className='text-[18px] border-r-2 border-black pr-4'>
+                    {item.title}
+                  </p>
+                  <p
+                    className={
+                      item.status === 'Active'
+                        ? 'text-[14px] font-semibold text-[#5583C3]'
+                        : 'text-[14px] font-semibold text-gray-400'
+                    }
+                  >
+                    {item.status}
+                  </p>
+                </div>
+                <div className='flex gap-3 flex-wrap '>
+                  <button className='flex gap-2 items-center bg-[#DFDFDF] py-2 px-2 rounded-lg'>
+                    Request delete
+                    <RiDeleteBinLine />
+                  </button>
+                  {item.status === 'Not Active' && (
+                    <button className='flex gap-2 items-center bg-[#5583C3] text-white py-2 px-3 rounded-lg'>
+                      Activate
+                    </button>
+                  )}
+                  <button className='flex gap-2 items-center bg-[#C8C8C8] py-2 px-2 rounded-lg'>
+                    See more
+                    <MdOutlineKeyboardArrowDown />
+                  </button>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
 
-      <Pagination
-        postsPerPage={postsPerPage} 
-        totalPosts={projectList.length} 
-        currentPage={currentPage}
-        paginateBack={paginateBack}
-        paginateFront={paginateFront}
-        paginate={paginate} 
-      />
+          <Pagination
+            postsPerPage={postsPerPage}
+            totalPosts={projects?.length}
+            currentPage={currentPage}
+            paginateBack={paginateBack}
+            paginateFront={paginateFront}
+            paginate={paginate}
+          />
+        </>
+      )}
 
       {/* <Link href='#' className="flex gap-2 items-center bg-[#205584] text-white mt-8 w-fit py-3 px-4 rounded-lg">
         See more
         <MdOutlineKeyboardArrowDown />
       </Link> */}
     </div>
-  );
-};
+  )
+}
 
-export default ExistingProject;
+export default ExistingProject
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/app/dashboard/projects/page.tsx
+++ src/app/dashboard/projects/page.tsx
@@ -13,6 +13,96 @@ export default function Project() {
   }, [])
   
   return (
-    <div>Project</div>
+    <div>Projectjvyhfyi</div>
   )
 }
+
+// INITS
+
+// import React from 'react'
+// import { AppProps } from 'next/app'
+// import Layout from '../components/Layout'
+// import projectContext from './projectContext'
+// import { ProjectProvider } from './projectContext' // import your ProjectProvider
+
+// import '../styles/globals.css'
+
+// function MyApp({ Component, pageProps }: AppProps) {
+//   return (
+//     <ProjectProvider>
+//       <Layout>
+//         <Component {...pageProps} />
+//       </Layout>
+//     </ProjectProvider>
+//   )
+// }
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <ProjectProvider>
+//       <Component {...pageProps} />
+//     </ProjectProvider>
+//   )
+// }
+
+// export default MyApp
+// pages/_app.tsx
+// import { AppProps } from 'next/app';
+// import { ReactNode } from 'react';
+// import { SessionProvider } from 'next-auth/react';
+// import { ChakraProvider } from '@chakra-ui/react';
+// import { ProjectProvider } from './projectContext';
+
+// function MyApp({ Component, pageProps }: AppProps) {
+//   return (
+//     <SessionProvider session={pageProps.session}>
+//       <ChakraProvider>
+//         {/* Wrap your components with the ProjectProvider */}
+//         <ProjectProvider>
+//           <ComponentWrapper>
+//             <Component {...pageProps} />
+//           </ComponentWrapper>
+//         </ProjectProvider>
+//       </ChakraProvider>
+//     </SessionProvider>
+//   );
+// }
+
+// function ComponentWrapper({ children }: { children: ReactNode }) {
+//   // You can add any global context or layout here
+//   return <div>{children}</div>;
+// }
+
+// export default MyApp;
+
+// without session id
+// pages/_app.tsx
+// import React from 'react';
+// import { ProjectProvider } from '../contexts/ProjectContext';
+// import '../styles/globals.css';
+
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <ProjectProvider>
+//       <Component {...pageProps} />
+//     </ProjectProvider>
+//   );
+// }
+
+// export default MyApp;
+
+// pages/_app.tsx
+// pages/_app.tsx
+// import React from 'react';
+// import { AppProps } from 'next/app';
+// import { ProjectProvider } from './projectContext';
+// import '../styles/globals.css';
+
+// function MyApp({ Component, pageProps }: AppProps) {
+//   return (
+//     <ProjectProvider>
+//       <Component {...pageProps} />
+//     </ProjectProvider>
+//   );
+// }
+
+// export default MyApp;
