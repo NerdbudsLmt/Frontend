2ae81cb8c8eeb6234448ccce7869c1f084ccbc39
muhammod1 <muhammodholawale@gmail.com>
2024-01-01 18:33:30 +0100

    dashboard

--- src/app/dashboard/component/Dashboard.tsx
+++ src/app/dashboard/component/Dashboard.tsx
@@ -1,14 +1,92 @@
-'use client'
+"use client";
 
-import { DoughnutChart } from "@/components/Dashbord/DoughnutChart";
+import DoughnutChart from "@/components/Dashbord/DoughnutChart";
 import Projects from "@/components/Dashbord/Projects";
 import QuickSet from "@/components/Dashbord/QuickSet";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
 
 
+interface Project {
+  projectName: string;
+  paymentStatus: number;
+  callSchedule: string;
+  status: boolean;
+  projectPercentage: number;
+  _id: number;
+}
 
+export default function DashboardCom() {
+  const { data: session } = useSession();
+  const [loading, setLoading] = useState(false);
+  const [projects, setProjects] = useState<Project[]>([]);
 
-export default  function DashboardCom() {
-  
+  useEffect(() => {
+    const fetchUserProjects = async () => {
+      try {
+        const accessToken = session?.user?.accessToken ?? "";
+        if (!accessToken) {
+          console.error("Access token not available");
+          return;
+        }
+
+        const url =
+          "https://nerdbuds.onrender.com/api/v1/projects/userProjects";
+
+        const response = await fetch(url, {
+          method: "GET",
+          headers: {
+            // 'Content-Type': 'application/json',
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        // console.log("API Response:", response);
+        if (response.ok) {
+          setLoading(true);
+          const data = await response.json();
+          // console.log(data)
+          setProjects(data?.data?.projects);
+          setLoading(false);
+        } else {
+          console.error("Failed to fetch user projects");
+        }
+      } catch (error) {
+        console.error("An error occurred:", error);
+      }
+    };
+
+    fetchUserProjects();
+  }, [session]);
+
+  const countStatusAndPercentage = projects.reduce(
+    (acc, project) => {
+      if (project.projectPercentage === 100) {
+        acc.truePercentageCount += 1;
+      } else {
+        acc.falsePercentageCount += 1;
+      }
+
+      if (project.status === true) {
+        acc.trueStatusCount += 1;
+      } else {
+        acc.falseStatusCount += 1;
+      }
+
+      return acc;
+    },
+    {
+      truePercentageCount: 0,
+      falsePercentageCount: 0,
+      trueStatusCount: 0,
+      falseStatusCount: 0,
+    }
+  );
+
+  const running = countStatusAndPercentage.trueStatusCount
+  const finished = countStatusAndPercentage.truePercentageCount
+  const pending = countStatusAndPercentage.falseStatusCount
+
 
   return (
     <div>
@@ -39,14 +117,28 @@ export default  function DashboardCom() {
           <p className="text-lg font-bold underline ">
             Overall Project Reports{" "}
           </p>
-          <div className="flex justify- gap-1">
+          <div className="flex flex-wrap justify-start gap-1">
             <ul className="text-left mt-4 font-semibold pl-5 w-[170px] text-md list-disc">
-              <li className="text-[#2CB629]">3 running </li>
-              <li className="text-[#009CFF] my-2">10 finished</li>
-              <li className="text-[#D69E00]">1 pending</li>
-            </ul>
+              <li className="text-[#2CB629]">
+                {running} running{" "}
+              </li>
+              <li className="text-[#009CFF] my-2">
+                {" "}
+                {finished} finished
+              </li>
+              <li className="text-[#D69E00]">
+                {" "}
+                {pending} pending
+              </li>
+            </ul>       
 
-            <DoughnutChart />
+            <DoughnutChart
+              running={running}
+              finished={finished}
+              pending={pending}
+            />
+
+           
           </div>
         </div>
       </div>
--- src/components/Dashbord/Projects.tsx
+++ src/components/Dashbord/Projects.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import React, { useState } from "react";
 import Image from "next/image";
@@ -6,9 +6,8 @@ import { FiPlus, FiClock } from "react-icons/fi";
 import { useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { useToast } from "@chakra-ui/react";
-import axios from 'axios';
+import axios from "axios";
 import Link from "next/link";
-
 
 const project = [
   {
@@ -46,103 +45,117 @@ const meeting = [
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 interface Project {
-  projectName: string
-  paymentStatus: boolean
-  _id: number
+  projectName: string;
+  paymentStatus: number;
+  callSchedule: string;
+  status: boolean;
+  projectPercentage: number;
+  _id: number;
 }
 
 export default function Projects() {
   const { data: session } = useSession();
   const [loading, setLoading] = useState(false);
-  const [projects, setProjects] = useState<Project[]>([])
+  const [projects, setProjects] = useState<Project[]>([]);
   const [error, setError] = useState<string | null>(null);
   const toast = useToast();
 
   useEffect(() => {
     const fetchUserProjects = async () => {
       try {
-        const accessToken = session?.user?.accessToken ?? ''
-        console.log(accessToken)
-        if (!accessToken) {
-          console.error('Access token not available')
-          return
-        }
+        const accessToken = session?.user?.accessToken ?? "";
+        // if (!accessToken) {
+        //   // console.error("Access token not available");
+        //   return;
+        // }
 
-        const url = 'https://nerdbuds.onrender.com/api/v1/projects/userProjects'
+        const url =
+          "https://nerdbuds.onrender.com/api/v1/projects/userProjects";
 
         const response = await fetch(url, {
-          method: 'GET',
+          method: "GET",
           headers: {
             // 'Content-Type': 'application/json',
             Authorization: `Bearer ${accessToken}`,
           },
-        })
+        });
 
-        console.log('API Response:', response)
+        // console.log("API Response:", response);
         if (response.ok) {
-          setLoading(true)
-          const data = await response.json()
-          console.log(data)
-          setProjects(data?.data?.projects)
-        setLoading(false)
-
+          setLoading(true);
+          const data = await response.json();
+          // console.log(data)
+          setProjects(data?.data?.projects);
+          setLoading(false);
         } else {
-          console.error('Failed to fetch user projects')
+          console.error("Failed to fetch user projects");
         }
       } catch (error) {
-        console.error('An error occurred:', error)
+        console.error("An error occurred:", error);
       }
-    }
-    // setLoading(false)
+    };
 
-    fetchUserProjects()
-  }, [session])
+    fetchUserProjects();
+  }, [session]);
 
+
+  
 
   return (
     <div className="my-7">
       <div className="text-[#265D80]  grid grid-cols-1 laptop_l:grid-cols-2 gap-5 ">
         <div className=" bg-[#F5F4F4] p-4 rounded-lg">
           <p className="text-lg font-bold">Projects </p>
+         
 
-         {/* {loading ? */}
-         {projects?.length === 0 ? 
-         (
-          <p className="text-app-pblue py-4 text-center text-lg font-bold">Loading...</p> 
-          )
-          :
-          (
-
-         <ul className="mt-4  text-white list-decimal  text-md">
-           {loading ?
-            <p className="text-app-pblue py-4 text-center text-lg font-bold">No project available</p> 
-            :
-          <>  {projects?.map((item) => (
-              <li
-                key={item._id}
-                className="flex items-center rounded-lg py-3 px-3 gap-4 my-3 bg-app-pblue"
-              >
-                <p className="text-[18px]">{item._id}</p>
-                <p className="text-[18px] border-r-2 pr-4">{item.projectName}</p>
-                <p
-                  className={
-                    item.paymentStatus === true
-                      ? "text-[12px] text-green-600"
-                      : "text-[12px] text-yellow"
-                  }
-                >
-                  {item.paymentStatus === true ? 'Finished'  : 'In Progress'}
+          {/* {loading ? */}
+          {projects?.length === 0 ? (
+            <p className="text-app-pblue py-4 text-center text-lg font-bold">
+              Loading...
+            </p>
+          ) : (
+            <ul className="mt-4  text-white list-decimal  text-md">
+              {loading ? (
+                <p className="text-app-pblue py-4 text-center text-lg font-bold">
+                  No project available
                 </p>
-              </li>
-            ))}
-            </>
-          }
-          </ul>
-          )
-          }
+              ) : (
+                <>
+                  {" "}
+                  {projects
+                    .slice(-3)
+                    .reverse()
+                    .map((item, index) => (
+                      <li
+                        key={item._id}
+                        className="flex items-center rounded-lg py-3 px-3 gap-4 my-3 bg-app-pblue"
+                      >
+                        <p className="text-[18px]">{index + 1}.</p>
+                        <p className="text-[18px] border-r-2 pr-4">
+                          {item.projectName}
+                        </p>
+                        <p
+                          className={
+                            item.paymentStatus === 100
+                              ? "text-[12px] text-green-600"
+                              : "text-[12px] text-yellow"
+                          }
+                        >
+                          {item.paymentStatus === 100
+                            ? "Finished"
+                            : "In Progress"}
+                        </p>
+                      </li>
+                    ))}
+                </>
+              )}
+            </ul>
+          )}
 
-
-          <Link href='/dashboard/projects/create' className="flex gap-2 items-center transition text-app-pblue bg-yellow border-2 border-yellow py-2 px-4 w-fit rounded-full">
+          <Link
+            href="/dashboard/projects/create"
+            className="flex gap-2 items-center transition text-app-pblue bg-yellow border-2 border-yellow py-2 px-4 w-fit rounded-full"
+          >
             <FiPlus size={20} />
             Create new project
           </Link>
@@ -161,15 +174,47 @@ export default function Projects() {
                 <p className="text-[.8rem]">Host: {item.host}</p>
               </div>
             ))} */}
+              {projects?.length === 0 ? (
+            <p className="text-app-pblue py-4 text-center text-lg font-bold">
+              Loading...
+            </p>
+          ) : (
+            <ul className="mt-4  text-white list-decimal  text-md">
+              {loading ? (
+                <p className="text-app-pblue py-4 text-center text-lg font-bold">
+                  No project available
+                </p>
+              ) : (
+                <>
+                  {" "}
+                  {projects
+                    .slice(-3)
+                    .reverse()
+                    .map((item, index) => (
+                      <div
+                      key={item._id}
+                      className="rounded-lg py-3.5 px-3 my-2 bg-app-pblue"
+                    >
+                      <p className=" text-app-sblue">{item.projectName}</p>
+                      <p className="text-[.8rem] ">{item.callSchedule}</p>
+                      <p className="text-[.8rem]">Host: Nerdbuds Ltd</p>
+                    </div>
+                    ))}
+                </>
+              )}
+            </ul>
+          )}
           </div>
 
-          <button className="flex gap-2 items-center text-app-pblue transition bg-yellow border-2 border-yellow mt-2 py-2 px-4 rounded-full">
+          <Link
+            href="/dashboard/support"
+            className="flex gap-2 items-center transition text-app-pblue bg-yellow border-2 border-yellow py-2 px-4 w-fit rounded-full"
+          >
             Schedule meeting
             <FiClock size={20} />
-          </button>
+          </Link>
         </div>
       </div>
     </div>
   );
 }
-
