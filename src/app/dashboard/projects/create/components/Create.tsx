9cf169d60316ef07d4f6a751c50567c29b83d82e
muhammod1 <muhammodholawale@gmail.com>
2024-01-01 15:15:36 +0100

    uncompleted create project

--- src/app/dashboard/projects/create/components/Create.tsx
+++ src/app/dashboard/projects/create/components/Create.tsx
@@ -1,89 +1,67 @@
 'use client'
 
-import React from "react";
+import React, { FormEvent, useState } from "react";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { FiArrowRight } from "react-icons/fi";
 import { LuClock3 } from "react-icons/lu";
 import { useSession } from "next-auth/react";
 import { useToast } from "@chakra-ui/react";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import axios from "axios";
+
 
 interface FormValues {
+  projectName: string;
   description: string;
-  items: string[];
-  dayOfWeek: string;
-  time: string;
-  meredian: string;
+  services: string[];
+  callSchedule: Date | null;
 }
 
-const itemsList: string[] = [
+
+const servicesList: string[] = [
   "Cloud Engineering",
   "UI/UX",
   "Security",
   "Consultation",
   "Branding",
-]; // Your list of items
-
-const timeOptions = [
-  "1:00",
-  "2:00",
-  "3:00",
-  "4:00",
-  "5:00",
-  "6:00",
-  "7:00",
-  "8:00",
-  "9:00",
-  "10:00",
-  "11:00",
-  "12:00",
 ];
 
-const meredian = ["AM", "PM"];
-
 const validationSchema = Yup.object().shape({
+  projectName: Yup.string().required("Project name is required"),
   description: Yup.string().required("Description is required"),
-  items: Yup.array().min(1, "Select at least one item"),
-  dayOfWeek: Yup.string().required("Day of the week is required"),
-  time: Yup.string().required("Time is required"),
-  meredian: Yup.string().required("Meredian is required"),
+  services: Yup.array().min(1, "Select at least one item"),
+  callSchedule: Yup.date().required("Select a fixed date and time"),
 });
 
 export default function CreateProject() {
   const { data: session }: any = useSession();
-  // console.log(session)
   const toast = useToast();
-  const handleSubmit = (values: FormValues) => {
-    console.log(values); // Log form values to console
+
+  const initialValues: FormValues = {
+    projectName: "",
+    description: "",
+    services: [],
+    callSchedule: null,
   };
 
-  async function createProject() {
-    const url = "https://nerdbuds.onrender.com/api/v1/projects";
-    // const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-
-    // console.log("Access Token:", session?.user?.accessToken);
-
-    const requestData = {
-      projectName: "IT",
-      services: ["Commerce", "Marine"],
-      callSchedule: "2023-10-27 21:20:09.431",
-      description: "I want my app in 3 months",
-    };
-
+  const handleFormSubmit = async (values: FormValues) => {
+    console.log(values)
     try {
-      const response = await fetch(url, {
-        method: "POST",
+      const response = await fetch('https://nerdbuds.onrender.com/api/v1/projects', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${session?.user?.accessToken}`, // Include the bearer token here
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${session?.user?.accessToken}`,
         },
-        body: JSON.stringify(requestData),
+        body: JSON.stringify(values),
       });
 
       if (response.ok) {
         const responseData = await response.json();
         toast({
-          title: "Project created ",
+          title: "Project created",
           description: responseData.message,
           status: "success",
           duration: 5000,
@@ -92,24 +70,21 @@ export default function CreateProject() {
         });
         console.log(responseData);
       } else {
-        toast({
-          title: "UNABLE TO CREATE NEW PROJECT, PLS CONTACT THE SUPPORT TEAM",
-          status: "error",
-          duration: 5000,
-          isClosable: true,
-          position: "top-right",
-        });
-        console.log("Create project error");
+        // toast({
+        //   title: response,
+        //   status: "error",
+        //   duration: 5000,
+        //   isClosable: true,
+        //   position: "top-right",
+        // });
+        console.error(response);
       }
     } catch (error) {
       console.error("An error occurred:", error);
     }
-  }
-
-  const handleButtonClick = () => {
-    createProject();
   };
 
+ 
   return (
     <div className="max-w-[800px]">
       <div className="flex gap-2 mb-8 items-center">
@@ -121,80 +96,89 @@ export default function CreateProject() {
       <p className="font-bold text-2xl my-6">Create a project</p>
 
       <Formik
-        initialValues={{
-          description: "",
-          items: [],
-          dayOfWeek: "",
-          time: "",
-          meredian: "",
-        }}
+        initialValues={initialValues}
         validationSchema={validationSchema}
-        onSubmit={handleSubmit}
+        onSubmit={(values) => handleFormSubmit(values)}
       >
-        <Form>
-          <div className="flex flex-col gap-2">
-            {/* <label>Description</label> */}
-            <Field
-              className="bg-[#F5F4F4] py-3 px-4 min-h-[130px] rounded-xl focus:outline-app-pblue focus:outline-1"
-              placeholder="Briefly describe your project."
-              as="textarea"
-              name="description"
-            />
-            <ErrorMessage
-              name="description"
-              component="div"
-              className="error text-red-500 italic text-sm"
-            />
-          </div>
-
-          <div className="my-6 bg-[#F5F4F4] py-4 px-4 rounded-xl">
-            <label className="font-semibold mb-4">
-              What services do you need?
-            </label>
-            <div className="flex gap-5 flex-wrap mt-4">
-              {itemsList.map((item) => (
-                <div
-                  key={item}
-                  className="flex bg-[#DCEBFF] py-2 px-4 rounded-xl gap-2 accent-app-pblue"
-                >
-                  <Field type="checkbox" name="items" value={item} />
-                  <label>{item}</label>
-                </div>
-              ))}
+        {({ values, handleChange, setFieldValue }) => (
+          <Form>
+            <div className="flex flex-col gap-2">
+              <Field
+                className="bg-[#F5F4F4] py-3 px-4 mb-8 rounded-xl focus:outline-app-pblue focus:outline-1"
+                placeholder="Project Name"
+                as="input"
+                name="projectName"
+              />
+              <ErrorMessage
+                name="description"
+                component="div"
+                className="error text-red-500 italic text-sm"
+              />
             </div>
-            <ErrorMessage
-              name="items"
-              component="div"
-              className="error text-red-500 italic text-sm"
+            <div className="flex flex-col gap-2">
+              <Field
+                className="bg-[#F5F4F4] py-3 px-4 min-h-[130px] rounded-xl focus:outline-app-pblue focus:outline-1"
+                placeholder="Briefly describe your project."
+                as="textarea"
+                name="description"
+              />
+              <ErrorMessage
+                name="description"
+                component="div"
+                className="error text-red-500 italic text-sm"
+              />
+            </div>
+
+            <div className="my-6 bg-[#F5F4F4] py-4 px-4 rounded-xl">
+              <label className="font-semibold mb-4">
+                What services do you need?
+              </label>
+              <div className="flex gap-5 flex-wrap mt-4">
+                {servicesList.map((item) => (
+                  <div
+                    key={item}
+                    className="flex bg-[#DCEBFF] py-2 px-4 rounded-xl gap-2 accent-app-pblue"
+                  >
+                    <Field type="checkbox" name="items" value={item} />
+                    <label>{item}</label>
+                  </div>
+                ))}
+              </div>
+              <ErrorMessage
+                name="items"
+                component="div"
+                className="error text-red-500 italic text-sm"
+              />
+            </div>
+
+            <p className="my-4 text-[18px] ">
+              Schedule a meeting through our Calendly to give us more information
+              about what project you have in mind.
+            </p>
+
+            <DatePicker
+              selected={values.callSchedule}
+              onChange={(date: Date | null) => setFieldValue('callSchedule', date)}
+              showTimeSelect
+              timeFormat="HH:mm"
+              timeIntervals={15}
+              dateFormat="Pp"
+              placeholderText="Select Date and Time"
+              className=" mt-2 border-2  rounded-lg w-fit py-3 px-3"
+              required
             />
-          </div>
 
-          <p className="my-4 text-[18px] ">
-            Schedule a meeting through our Calendly to give us more information
-            about what project you have in mind.
-          </p>
-          <div>
-            <button
-              type="submit"
-              className="flex gap-3 items-center transition bg-[#3F9BD5] border-2 text-white py-3 px-4 rounded-lg"
-            >
-              Schedule a meeting with Calendly
-              <LuClock3 size={20} className="font-bold" />
-            </button>
-          </div>
-          <p className="my-4 text-[18px] ">Schedule a meeting with Calendly</p>
-
-          <div>
-            <button
-              type="submit"
-              className="flex gap-3 mt-4 items-center transition bg-[#205584] border-2 text-white border-[#205584] py-2 px-4 rounded-lg"
-              onClick={handleButtonClick}
-            >
-              Create Project
-              <FiArrowRight size={20} />
-            </button>
-          </div>
-        </Form>
+            <div>
+              <button
+                type="submit"
+                className="flex gap-3 mt-4 items-center transition bg-[#205584] border-2 text-white border-[#205584] py-2 px-4 rounded-lg"
+              >
+                Create Project
+                <FiArrowRight size={20} />
+              </button>
+            </div>
+          </Form>
+        )}
       </Formik>
     </div>
   );
