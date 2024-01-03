a8959be448651873d5e4cd9165c9f88736a3570e
Samuel <Samueladeyemi2006@gmail.com>
2024-01-03 22:21:02 +0100

    feat: project progress done 84%

--- src/app/dashboard/projects/progress/page.tsx
+++ src/app/dashboard/projects/progress/page.tsx
@@ -1,34 +1,19 @@
-"use client";
-
-import ProjectProgress from "@/components/Dashbord/ProjectProgress";
-import React, { useState } from "react";
-import { BsSliders2Vertical } from "react-icons/bs";
-import {
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  Button
-} from "@chakra-ui/react";
-import Pagination from "@/components/Pagination";
-import { getServerSession } from "next-auth/next"
-import { redirect } from "next/navigation"
-import { options } from "..//../../api/auth/[...nextauth]/options"
+import React from "react";
+import { getServerSession } from "next-auth/next";
+import { redirect } from "next/navigation";
+import { options } from "..//../../api/auth/[...nextauth]/options";
 import ProProgress from "./component/ProProgress";
 
+export default async function Progress() {
+  const session = await getServerSession(options);
 
-
-export default async function Progress() {  
-  const  session  = await getServerSession(options);
-
-  if(!session) {
-    redirect('/api/auth/signin?callbackUrl=/server')
+  if (!session) {
+    redirect("/api/auth/signin?callbackUrl=/server");
   }
- 
+
   return (
-    <div className="">
+    <div>
       <ProProgress />
-    
     </div>
   );
 }
