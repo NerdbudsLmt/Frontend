810c1f9d332ff4c88c39ae093dd7f615febe85a2
muhammod1 <muhammodholawale@gmail.com>
2024-01-05 09:20:54 +0100

    dashboard

--- src/app/dashboard/component/Dashboard.tsx
+++ src/app/dashboard/component/Dashboard.tsx
@@ -3,23 +3,68 @@
 import DoughnutChart from "@/components/Dashbord/DoughnutChart";
 import Projects from "@/components/Dashbord/Projects";
 import QuickSet from "@/components/Dashbord/QuickSet";
-import { useState, useEffect } from "react";
+import { useState, useEffect, SetStateAction } from "react";
 import { useSession } from "next-auth/react";
-
+import Link from "next/link";
+import { FiPlus } from "react-icons/fi";
 
 interface Project {
+  _id: string;
+  user: string;
   projectName: string;
-  paymentStatus: number;
+  services: string[];
   callSchedule: string;
-  status: boolean;
   projectPercentage: number;
-  _id: number;
+  status: boolean;
+  paymentStatus: boolean;
+  createdAt: string;
+  completedDate: string;
 }
 
 export default function DashboardCom() {
   const { data: session } = useSession();
   const [loading, setLoading] = useState(false);
+  const [activeProjects, setActiveProjects] = useState<Project[]>([]);
+  const [error, setError] = useState<string | null>(null);
   const [projects, setProjects] = useState<Project[]>([]);
+  const [selectedProject, setSelectedProject] = useState(null); // New state to keep track of selected project
+  const [selectedProjectIndex, setSelectedProjectIndex] = useState(0);
+
+  useEffect(() => {
+    const fetchActiveProjects = async () => {
+      try {
+        const accessToken = session?.user?.accessToken ?? "";
+        const url = `https://nerdbuds.onrender.com/api/v1/projects/activeuserProjects`;
+
+        const response = await fetch(url, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        if (response.ok) {
+          setLoading(true);
+          const data = await response.json();
+          const filteredProjects = data?.data?.projects.filter(
+            (project: { status: boolean }) => !project.status
+          );
+          setActiveProjects(filteredProjects);
+
+          console.log(filteredProjects);
+          setLoading(false);
+        } else {
+          console.error("Failed to fetch active");
+          setError("Failed to fetch active");
+        }
+      } catch (error) {
+        console.error("An error occurred:", error);
+        setError("An error occurred while fetching active projects");
+      }
+    };
+
+    fetchActiveProjects();
+  }, [session]);
 
   useEffect(() => {
     const fetchUserProjects = async () => {
@@ -83,33 +128,114 @@ export default function DashboardCom() {
     }
   );
 
-  const running = countStatusAndPercentage.trueStatusCount
-  const finished = countStatusAndPercentage.truePercentageCount
-  const pending = countStatusAndPercentage.falseStatusCount
+  const running = countStatusAndPercentage.trueStatusCount;
+  const finished = countStatusAndPercentage.truePercentageCount;
+  const pending = countStatusAndPercentage.falseStatusCount;
 
+  const handleProjectClick = (index: SetStateAction<number>) => {
+    // setSelectedProject(activeProjects[index]);
+    setSelectedProjectIndex(index);
+  };
+
+  function formatDateString(dateString: string | number | Date) {
+    try {
+      const date = new Date(dateString);
+      if (isNaN(date.getTime())) {
+        throw new Error("Invalid date string");
+      }
+
+      const day = date.getDate();
+      const month = new Intl.DateTimeFormat("en-US", { month: "long" }).format(
+        date
+      );
+      const year = date.getFullYear();
+
+      const suffix = getOrdinalSuffix(day);
+
+      return `${day}${suffix} of ${month} ${year}`;
+    } catch (error) {
+      console.error("Error formatting date:", error);
+      return "Invalid Date";
+    }
+  }
+
+  function getOrdinalSuffix(number: number) {
+    const suffixes = ["th", "st", "nd", "rd"];
+    const v = number % 100;
+    return suffixes[(v - 20) % 10] || suffixes[v] || suffixes[0];
+  }
 
   return (
     <div>
       <div className="text-[#265D80]  flex flex-wrap gap-4 justify-between items-start">
         <div className=" bg-[#F5F4F4] p-3 basis-full lg:basis-[48%]   text-center rounded-lg">
           <p className="text-lg font-bold underline ">Active Projects </p>
-          <div className="flex justify-between">
-            <div className="flex justify-between items-start gap-3 mt-3 h-fit">
-              <p className="text-[5rem] text-[#132E40] -mt-8 h-fit font-extrabold">
-                1
+          <div className="flex justify-between gap-4 flex-wrap">
+            {loading ? (
+              <p className="text-app-pblue py-4 text-center text-lg font-bold">
+                Loading...
               </p>
-              <p className=" px-3.5 py-1.5 h-fit rounded-full  border-dotted border-2 border-app-pblue">
-                2
-              </p>
-              <p className="text-xl font-bold px-3.5 py-1.5 h-fit rounded-full  border-dotted border-2 border-app-pblue">
-                +
-              </p>
-            </div>
-            <ul className="text-left mt-4 pl-5 text-md list-disc">
-              <li className="">Name: Project Assistant</li>
-              <li>Status: 97% Complete</li>
-              <li>Deadline: 10th August 2023</li>
-            </ul>
+            ) : (
+              <div>
+                {activeProjects.length === 0 ? (
+                  <>
+                    <p className="text-app-pblue py-4 text-center text-lg font-bold">
+                      No active projects
+                    </p>
+                  
+                  </>
+                ) : (
+                  <div className="flex justify-between">
+                    {activeProjects.slice(0, 2).map((item, index) => (
+                      <div
+                        key={item._id}
+                        className={`flex justify-between items-start gap-3 mt-3 h-fit cursor-pointer ${
+                          selectedProject === item ? "bg-gray-300" : ""
+                        }`}
+                        onClick={() => handleProjectClick(index)}
+                      >
+                        <p
+                          className={` ${
+                            selectedProjectIndex === index
+                              ? "text-[5rem] text-[#132E40] -mt-8 h-fit font-extrabold"
+                              : " text-xl font-bold px-3.5 py-1.5 h-fit rounded-full  border-dotted border-2 border-app-pblue"
+                          }`}
+                        >
+                          {index + 1}
+                        </p>
+                      </div>
+                    ))}
+                    <Link
+                      href="/dashboard/projects/create"
+                      className="text-xl ml-2 font-bold px-2.5 py-2.5 h-fit mt-3 rounded-full  border-dotted border-2 border-app-pblue"
+                    >
+                      <FiPlus size={20} />
+                    </Link>
+                  </div>
+                )}
+              </div>
+            )}
+
+            {activeProjects.length > 0 && (
+              <div className="mt-4 text-left pl-5 text-md">
+                <ul className="list-disc">
+                  <li>
+                    Name: {activeProjects[selectedProjectIndex].projectName}
+                  </li>
+                  <li>
+                    Status:{" "}
+                    {activeProjects[selectedProjectIndex].projectPercentage}%
+                    Complete
+                  </li>
+                  <li>
+                    Deadline:{" "}
+                    {formatDateString(
+                      activeProjects[selectedProjectIndex].completedDate
+                    )}
+                  </li>
+                </ul>
+              </div>
+            )}
           </div>
         </div>
 
@@ -119,26 +245,16 @@ export default function DashboardCom() {
           </p>
           <div className="flex flex-wrap justify-start gap-1">
             <ul className="text-left mt-4 font-semibold pl-5 w-[170px] text-md list-disc">
-              <li className="text-[#2CB629]">
-                {running} running{" "}
-              </li>
-              <li className="text-[#009CFF] my-2">
-                {" "}
-                {finished} finished
-              </li>
-              <li className="text-[#D69E00]">
-                {" "}
-                {pending} pending
-              </li>
-            </ul>       
+              <li className="text-[#2CB629]">{running} running </li>
+              <li className="text-[#009CFF] my-2"> {finished} finished</li>
+              <li className="text-[#D69E00]"> {pending} pending</li>
+            </ul>
 
             <DoughnutChart
               running={running}
               finished={finished}
               pending={pending}
             />
-
-           
           </div>
         </div>
       </div>
