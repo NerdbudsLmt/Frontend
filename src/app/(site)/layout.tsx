0161eddf0c93ea8824be4dc66e17bc7409446be6
muhammod1 <muhammodholawale@gmail.com>
2024-01-15 22:44:25 +0100

    adjustment

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -13,9 +13,9 @@ export default function Home({
 }) {
   return (
     <section className="mx-auto text-white w-[97%] tablet:w-[95%] max-w-[1380px]">
-      {/* <Nav /> */}
+      <Nav />
       {children}
-      <Footer />
+      {/* <Footer /> */}
     </section>
   );
 }
