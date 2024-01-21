aae25d2364cac307c8dff18c48e900e3f04bb15f
Samuel <Samueladeyemi2006@gmail.com>
2024-01-21 19:56:45 +0100

    feat: view more done

--- src/hooks/useFormattedDate.ts
+++ src/hooks/useFormattedDate.ts
@@ -1,26 +1,31 @@
 import { useEffect, useState } from "react";
 
-const useFormattedDate = (dateString: string) => {
+const useFormattedDate = (dateString: string | undefined) => {
   const [formattedDate, setFormattedDate] = useState<string | null>(null);
 
   useEffect(() => {
-    const options: Intl.DateTimeFormatOptions = {
-      weekday: "long",
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-      hour: "numeric",
-      minute: "numeric",
-      timeZone: "UTC",
-      hour12: true,
-    };
+    if (dateString) {
+      const options: Intl.DateTimeFormatOptions = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        timeZone: "UTC",
+        hour12: true,
+      };
 
-    const formattedDateString = new Intl.DateTimeFormat(
-      "en-US",
-      options
-    ).format(new Date(dateString));
+      const formattedDateString = new Intl.DateTimeFormat(
+        "en-US",
+        options
+      ).format(new Date(dateString));
 
-    setFormattedDate(formattedDateString);
+      setFormattedDate(formattedDateString);
+    } else {
+      // Handle the case where dateString is undefined or empty
+      setFormattedDate(null);
+    }
   }, [dateString]);
 
   return formattedDate;
